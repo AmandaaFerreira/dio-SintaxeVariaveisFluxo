2c4aceb1a07807913ba04eded982a79d10271ec7
Amanda Ferreira <amandaferreiradasilva33@gmail.com>
2022-01-09 17:30:25 -0300

    meu primeiro commit

---
+++ src/TestaConversao.java
@@ -0,0 +1,15 @@
+
+public class TestaConversao {
+
+	public static void main(String[] args) {
+		double salario = 1270.50;
+		int valor = (int) salario;
+		
+		System.out.println(valor);
+	
+		long numeroGrande = 1000000000000l;
+		
+		Long numero = 74647l;
+		System.out.println(numeroGrande);
+	}
+}
